651641e8d017b0af1af8cca14e8290113ebff4a9
Inpyo Lee <mynameispyo@gmail.com>
2023-09-23 00:51:34 -0400

    minor

--- extensions-builtin/Lora/scripts/lora_script.py
+++ extensions-builtin/Lora/scripts/lora_script.py
@@ -43,7 +43,7 @@ shared.options_templates.update(shared.options_section(('extra_networks', "Extra
 
 
 shared.options_templates.update(shared.options_section(('compatibility', "Compatibility"), {
-    "lora_functional": shared.OptionInfo(False, "Lora/Networks: use old method that takes longer when you have multiple Loras active and produces same results as kohya-ss/sd-webui-additional-networks extension"),
+    "lora_functional": shared.OptionInfo(False, "Lora/Networks: use old method that takes longer when you have multiple Loras active and produces same results as kohya-ss/sd-we-additional-networks extension"),
 }))
 
 
